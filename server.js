169b50c39e661183e158c4c1e3e7f7683170bebf
Katarzyna <kkazimierczuk@gmail.com>
2020-10-12 12:13:53 +0200

    add dlete endpoint

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/testimointials')));
 
 
 const db = [
@@ -14,34 +13,35 @@ const db = [
   { id: 3, author: 'Alicia Keys', text: 'They really know how to make you sad.' },
 ];
 
-app.get('/', (req, res) => {
+app.get('/testimonials', (req, res) => {
   res.json(db);
 });
 
-app.get('/:id', (req, res) => {
+app.get('/testimonials/:id', (req, res) => {
   res.json(db.find(item => item.id == req.params.id));
 });
 
-app.get('/random', (req, res) => {
-  res.json('db');
+app.get('/testimonials/random', (req, res) => {
+  res.json(db[Math.floor(Math.random() * db.length)]);
 });
 
-app.post('/', (req, res) => {
+app.post('/testimonials', (req, res) => {
   const { author, text } = req.body;
-  const id = Math.floor(Math.random() + 99);
+  const id = Math.floor(Math.random()*100 + 1);
   db.push({ id, author, text });
   res.json({ message: 'OK' });
 });
 
-app.put('/:id', (req, res) => {
+app.put('/testimonials/:id', (req, res) => {
+  const { author, text } = req.body;
   const testimonials = db.find(item => item.id == req.params.id);
-  testimonials.author = req.body.author;
-  testimonials.text = req.body.text;
+  testimonials.author = author;
+  testimonials.text = text;
   res.json({ message: 'OK' });
 });
 
-app.delete(':id', (req, res) => {
-  db.delete(item => item.id == req.params.id);
+app.delete('/testimonials/:id', (req, res) => {
+  db == db.filter((item) => item.id !== req.params.id);
   res.json({ message: 'OK' });
 });
 

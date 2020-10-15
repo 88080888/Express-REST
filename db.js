71f86bd6ca3e0ba4bc53d0dbb45a0f2f92da5ed0
Katarzyna <kkazimierczuk@gmail.com>
2020-10-15 16:28:49 +0200

    update

--- db.js
+++ db.js
@@ -5,14 +5,12 @@ let db = {
     { id: 3, author: 'Alicia Keys', text: 'They really know how to make you sad.' },
   ],
 
-
   concerts: [
     { id: 1, performer: 'John Doe', genre: 'Rock', price: 25, day: 1, image: '/img/uploads/1fsd324fsdg.jpg' },
     { id: 2, performer: 'Rebekah Parker', genre: 'R&B', price: 25, day: 1, image: '/img/uploads/2f342s4fsdg.jpg' },
     { id: 3, performer: 'Maybell Haley', genre: 'Pop', price: 40, day: 1, image: '/img/uploads/hdfh42sd213.jpg' },
   ],
 
-  
   seats: [
     { id: 1, day: 1, seat: 3, client: 'Amanda Doe', email: 'amandadoe@example.com' },
     { id: 2, day: 1, seat: 9, client: 'Curtis Johnson', email: 'curtisj@example.com'  },

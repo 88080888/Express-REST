25da8c9859267bee0038e60d92d0ebc079c6db3d
Katarzyna <kkazimierczuk@gmail.com>
2020-10-21 13:26:21 +0200

    update

--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
-const { v4: uuidv4 } = require('uuid');
+
+const ConcertController = require('../controllers/concerts.controller');
 
 router.get('/concerts', ConcertController.getAll);
 router.get('/concerts/:id', ConcertController.getById);
 router.post('/concerts', ConcertController.postNew);
 router.put('/concerts/:id', ConcertController.edit);
 router.delete('/concerts/:id', ConcertController.deleteById);
-
-
 
 module.exports = router;
--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,49 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
-const { v4: uuidv4 } = require('uuid');
 
-router.route('/seats').get((req, res) => {
-  res.json(db.seats);
-});
+const SeatController = require('../controllers/seats.controller');
 
-router.route('/seats/:id').get((req, res) => {
-  res.json(db.seats.find(item => item.id == req.params.id));
-});
-
-router.route('/seats').post((req, res) => {
-  const { day, seat, client, email } = req.body;
-  const payload = {
-    id: uuidv4(),
-    day: day,
-    seat: seat,
-    client: client,
-    email: email,
-  }
-
-  if (db.seats.some(item => (item.seat === payload.seat && item.day ===payload.day))) {
-    res.json({message: 'The slot is already taken'})
-  }
-  else {
-    db.seats.push(payload);
-    req.io.emit('seatsUpdated', db.seats);
-    res.json({ message: 'OK' });
-  }
-});
-
-router.route('/seats/:id').put((req, res) => {
-  const { day, seat, client, email } = req.body;
-  const seats = db.seats.find(item => item.id == req.params.id);
-  seats.day = day;
-  seats.seat = seat;
-  seats.client = client;
-  seats.email = email;
-  res.json({ message: 'OK' });
-});
-
-router.route('/seats/:id').delete((req, res) => {
-  db.seats = db.seats.filter((item) => item.id != req.params.id);
-  res.json({ message: 'OK' });
-});
+router.get('/seats', SeatController.getAll);
+router.get('/seats/:id', SeatController.getById);
+router.post('/seats', SeatController.postNew);
+router.put('/seats/:id', SeatController.edit);
+router.delete('/seats/:id', SeatController.deleteById);
 
 module.exports = router;
